6d8bece133381016371d189a0d60d76eeb5a7c8b
huanghz <710421059@qq.com>
2024-12-29 23:54:15 +0800

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,23 +2,20 @@
 import { ref } from 'vue'
 
 const count = ref(0)
-const version = '2.1.68'
+const version = '2.1.42'
 const dl = () => {
-	window.open('https://gitee.com/hhzxxx/exilence-next-tx-release/releases/download/gjf2/Easy-Farm-Setup-Latest.exe?t=' + Date.now())
+	window.open('https://gitee.com/hhzxxx/exilence-next-tx-release/releases/download/latest/Easy-Farm-Setup-Latest.exe?t=' + Date.now())
 }
 
 const dl2 = () => {
 	window.open(
-		`https://cf.981001.xyz/gjf2/Easy-Farm-Setup-Latest.exe?t=${Date.now()}`
+		`https://cf.981001.xyz/Easy-Farm-Setup-Latest.exe?t=${Date.now()}`
 	)
 }
 </script>
 
 <template>
-	<h1>{{ `poe2易刷查价器 最新版本${version}` }}</h1>
-	<div>
-		<img src="/2.1.68.png" />
-	</div>
+	<h1>{{ `易刷国服 最新版本${version}` }}</h1>
 	<div class="card">
 		<button style="background-color: bisque" type="button" @click="dl">
 			点击下载-分流1
@@ -28,15 +25,6 @@ const dl2 = () => {
 		<button style="background-color: bisque" type="button" @click="dl2">
 			点击下载-分流2
 		</button>
-	</div>
-	<div>
-		<p>列举几个常见问题：</p>
-		<p>
-			查价器覆盖层界面很小显示不全：将游戏的窗口大小来回调整一下，比如切窗口化然后再切回去
-		</p>
-		<p>一直限流太频繁：加速器公用IP导致的封锁，和我没关系</p>
-		<p>查价失败，网络问题看这个图解决：</p>
-		<img src="/image.png" />
 	</div>
 </template>
 
