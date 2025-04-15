929f1226f6cba61524151849f86f8244040bee89
hhzxxx <710421059@qq.com>
2025-04-15 18:01:53 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 
 const count = ref(0)
-const version = '2.1.81'
+const version = '2.1.82'
 const dl = () => {
 	window.open(`https://gitee.com/hhzxxx/exilence-next-tx-release/releases/download/gjf2/Easy-Farm-Setup-Latest.exe?t=` + Date.now())
 }
