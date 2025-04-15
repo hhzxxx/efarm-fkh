6ab0a614792fc3e689ee485ba61f197bb4c2b630
hhzxxx <710421059@qq.com>
2025-04-15 18:02:57 +0800

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const dl = () => {
 
 const dl2 = () => {
 	window.open(
-		`https://cf.981001.xyz/Easy-Farm-Setup-Latest.exe?t=${Date.now()}`
+		`https://cf.981001.xyz/gjf2/Easy-Farm-Setup-Latest.exe?t=${Date.now()}`
 	)
 }
 </script>
