fc674d6238ed311849bcf4b0573e8169c8a47238
hhzxxx <710421059@qq.com>
2025-04-27 21:57:08 +0800

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 
 const count = ref(0)
-const version = '2.1.82'
+const version = '2.1.84'
 const dl = () => {
 	// window.open(`https://release.710421059.xyz/file/Easy-Farm-Setup-Latest.exe`)
 	window.open(`https://pan.quark.cn/s/8ad405b474e9`)
