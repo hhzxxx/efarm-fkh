a3ff52b14220b2d600c7a99926f40cc17d6d26ae
huanghz <huanghz@hhz.com>
2025-11-03 13:26:38 +0800

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const mdContent = ref('')
 const releaseDate = ref('')
 
 const dl = () => {
-  window.open(`https://gh.072103.xyz/hhzxxx/efarm-fkh/releases/tag/gjf2/Easy-Farm-Setup-Latest.exe`)
+  window.open(`https://gh.072103.xyz/hhzxxx/efarm-fkh/releases/download/gjf2/Easy-Farm-Setup-Latest.exe`)
 }
 
 const dl2 = () => {
